2e15489f8b5b762e457e335ef093c47557e0b544
Jakob Voss <voss@gbv.de>
2012-10-08 11:25:37 +0200

    support streaming response

--- lib/Plack/Middleware/SuppressResponseCodes.pm
+++ lib/Plack/Middleware/SuppressResponseCodes.pm
@@ -4,10 +4,13 @@ package Plack::Middleware::SuppressResponseCodes;
 use strict;
 use parent qw(Plack::Middleware);
 
+use Plack::Util;
+
 sub call {
     my($self, $env) = @_;
     my $res = $self->app->($env);
-    $self->response_cb($res, sub {
+    Plack::Util::response_cb($res, sub {
+        my $res = shift;
         if ( $res->[0] =~ /^[45]../ and
              $env->{QUERY_STRING} =~ /(?:^|&)suppress_response_codes(=([^&]+))?/ 
              and !($1 and $2 =~ /^(0|false)$/) ) {
@@ -23,18 +26,23 @@ sub call {
     use Plack::Builder;
 
     builder {
-        enable 'SuppressResponseCodes'
-        $app
+        enable 'SuppressResponseCodes';
+        $app;
     };
 
 =head1 DESCRIPTION
 
-Plack::Middleware::SuppressResponseCodes sets the HTTP status code of a
-response to 200 if the response is an error (status code 4xx or 5xx) and the
-query parameter C<suppress_response_codes> is present with any value except
-C<0> or C<false>. This behaviour is useful for clients that cannot handle HTTP
-errors.  It has been implemented in popular APIs such as Twitter and Microsoft
-Live.
+Plack::Middleware::SuppressResponseCodes modifies error responses (PSGI
+response with HTTP status code 4xx or 5xx) if the query parameter
+C<suppress_response_codes> is present with any value except C<0> or C<false>.
+The status code is set to 200 in this case. This behaviour is useful for
+clients that cannot handle HTTP errors.  It has also been implemented in
+popular APIs such as Twitter and Microsoft Live.
+
+=head1 SEE ALSO
+
+One should think about embedding the status code in the response body when
+using this middleware, for instance with L<Plack::Middleware::JSONP::Headers>.
 
 =cut
 
